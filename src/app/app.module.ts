849409ff3724ae14cb150392b79c8f45ef9a1c72
Abdelouahed Ait-essaih <abdelouahed-maroc@live.fr>
2021-02-26 02:47:40 +0100

    Creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FavoriteComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
